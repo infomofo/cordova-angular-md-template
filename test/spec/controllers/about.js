2a9b0098fbdd668bcc2f7c63d664e9e2ff1c5c7b
Will Chiong <will@infomofo.com>
2015-02-08 18:44:18 -0500

    fix failing node tests

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('yoAngularChromeApp'));
+  beforeEach(module('yoAngularCordovaApp'));
 
   var AboutCtrl,
     scope;
@@ -16,7 +16,4 @@ describe('Controller: AboutCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
